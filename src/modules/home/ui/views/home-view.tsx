cfc5804b7ef2fe10549a102c16df40691f106600
teddyjeremy <teddyjeremy12@gmail.com>
2025-10-18 19:44:22 +0300

    09: agent setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const {data} = useQuery(trpc.hello.queryOptions({ text: "Jeremy" }));
+  
   return (
     <div>
-      <p>{data?.greeting}</p>
+      Home View
     </div>
   )
 }
