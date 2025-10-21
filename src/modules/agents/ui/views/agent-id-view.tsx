526a4acb6a545df1f2ad7050b11fda32937f5862
teddyjeremy <teddyjeremy12@gmail.com>
2025-10-21 14:03:57 +0300

    15: agent update delete

--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -1,28 +1,68 @@
 "use client";
 import { ErrorState } from "@/components/error-state";
+import { useState } from "react";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { AgentIdViewHeader } from "../components/agent-id-view-header";
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { Badge } from "@/components/ui/badge";
 import { VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useConfirm } from "@/hooks/use-confirm";
+import { UpdateAgentDialog } from "../components/update-agent-dialog";
 
 interface Props {
     agentId : string;
 };
 
 export const AgentIdView =({agentId}: Props) =>{
+    const router = useRouter();
+    const queryClient = useQueryClient();
+
+    const [updatedAgentDialogOpen, setUpdateAgentDialogOpen] = useState(false);
+
     const trpc= useTRPC();
     const {data} = useSuspenseQuery(trpc.agents.getOne.queryOptions({id: agentId}))
+    const removeAgent = useMutation(
+      trpc.agents.remove.mutationOptions({
+        onSuccess: async () =>{
+          await queryClient.invalidateQueries(trpc.agents.getMany.queryOptions({}))
+          router.push("/agents")
+        },
+        onError: (error) => {
+          toast.error(error.message);
+          
+        },
+      }),
+    );
 
+    const [RemoveConfirmation, confirmRemove] = useConfirm(
+      "Are you sure?",
+      `The following action will remove ${data.meetingCount} associated meetings`,
+    );
+    const handleRemoveAgent = async () =>{
+       const ok = await confirmRemove();
+
+       if(!ok) return;
+
+       await removeAgent.mutateAsync({id: agentId});
+    }
     return(
+      <>
+        <RemoveConfirmation />
+        <UpdateAgentDialog 
+        open={updatedAgentDialogOpen}
+        onOpenChange={setUpdateAgentDialogOpen} 
+        initialValues={data}
+        />
         <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
             <AgentIdViewHeader 
             agentId ={agentId}
             agentName ={data.name}
-            onEdit={()=>{}}
-            onRemove={()=>{}}
+            onEdit={()=>setUpdateAgentDialogOpen(true)}
+            onRemove={handleRemoveAgent}
             />
             <div className="bg-white rounded-lg border">
                 <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
@@ -48,6 +88,7 @@ export const AgentIdView =({agentId}: Props) =>{
                 </div>
             </div>
         </div>
+        </>
     );
 };
 
